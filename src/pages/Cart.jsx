00cf639d14d98667c3dbdbc15ba21cd68a63c4ee
Anna  Leoshko <anna.leoshko@gmail.com>
2023-06-01 19:29:32 +0200

    implemented cart page and CartSlice

---
+++ src/pages/Cart.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+
+import CartItem from '../components/CartItem';
+import { clearItems } from '../redux/slices/cartSlice';
+
+export default function Cart() {
+    const dispatch = useDispatch();
+    const { totalPrice, items } = useSelector((state) => state.cart);
+    const totalCount = items.reduce((sum, item) => sum + item.count, 0);
+
+    const onClickClear = () => {
+        if (window.confirm('Do you want clear the cart?')) {
+            dispatch(clearItems());
+        }
+    }
+
+    return (
+        <div className="CartContainer">
+            <div className="cart-header">
+                <h3 className="Heading">Shopping Cart</h3>
+                <button onClick={onClickClear} className="Action">Remove all</button>
+            </div>
+            <div className='cart-items'>
+                {items.map((item) => (
+                    <CartItem key={item.id} {...item} />
+                ))}
+            </div>
+            <hr/>
+            <div className="checkout">
+                <div className="total">
+                    <div className="subtotal">
+                        Subtotal ({totalCount} items): ${totalPrice}
+                    </div>
+                </div>
+                <button className="button">Checkout</button>
+                <Link to="/"><button className="button">Go back to the main page</button></Link>
+            </div>
+        </div>
+    )
+}

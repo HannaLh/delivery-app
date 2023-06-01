e8ea9db8b1b5b254c817fe8377fd32fb2caaf89c
Anna  Leoshko <anna.leoshko@gmail.com>
2023-06-01 15:05:21 +0200

    added categories folder

---
+++ src/components/ShopCategories/ShopCategories.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './ShopCategories.css'
+
+const categories = ['McDonalds', 'KFC', 'Burgerking', 'Starbucks', 'PizzaHut', 'SaladStory']; 
+
+export default function ShopCategories({ value, onChangeCategory }) {
+    return (
+        <div className="categories">
+                <ul>
+                    {categories.map((categoryName, i) => (
+                        <li
+                            key={i}
+                            onClick={() => onChangeCategory(i)}
+                            className={value === i ? 'active' : ""}>
+                            {categoryName}
+                        </li>
+                    ))
+                    }
+                </ul>
+            </div>
+    )
+}

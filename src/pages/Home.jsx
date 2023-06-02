601f831cc7d45e1bc853d4c9564f6d81c6357cf8
Anna  Leoshko <anna.leoshko@gmail.com>
2023-06-02 11:16:12 +0200

    refactored categories function

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import axios from 'axios';
 
 import Header from '../components/Header/Header';
-import ShopCategories from '../components/ShopCategories/ShopCategories';
+import ShopCategories from '../components/ShopCategories/ShopCategories'
 import { setCategoryId } from '../redux/slices/filterSlice';
 import { ProductBlock } from '../components/ProductsBlock/ProductBlock';
 import LoadingSpinner from '../components/ProductsBlock/LoadingSpinner';
@@ -11,12 +11,11 @@ import LoadingSpinner from '../components/ProductsBlock/LoadingSpinner';
 
 export default function Home() {
     const dispatch = useDispatch();
-    const categoryId = useSelector(state => state.filter.categoryId);
 
     const [items, setItems] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
 
-    const category = categoryId > 0 ? `category=${categoryId}` : '';
+    const {categoryId} = useSelector((state) =>state.filter);
 
     const onChangeCategory = (id) => {
         dispatch(setCategoryId(id))
@@ -24,6 +23,7 @@ export default function Home() {
 
     useEffect(() => {
         setIsLoading(true);
+        const category = categoryId > 0 ? `category=${categoryId}` : '';
         axios.get(`https://645513ffa74f994b3351784a.mockapi.io/food-positions?${category}`)
             .then(res => {
                 setItems(res.data);
@@ -33,7 +33,7 @@ export default function Home() {
     }, [categoryId])
 
     const products = items.map((obj) => <ProductBlock key={obj.id}{...obj} />);
-    const loading = [...new Array(6)].map((_, index) => <LoadingSpinner key={index} />);
+    const loading = <LoadingSpinner/>;
 
     return (
         <div>
@@ -41,7 +41,7 @@ export default function Home() {
             <div className='container food-container'>
                 <div className='products'>
                     <div className='products-categories'>
-                        <ShopCategories value={categoryId} onChangeCategory={(i) => setCategoryId(i)}/>
+                        <ShopCategories value={categoryId} onChangeCategory={onChangeCategory}/>
                     </div> 
                 </div>
                 <div className="card-flex"> {isLoading ? loading : products} </div>
